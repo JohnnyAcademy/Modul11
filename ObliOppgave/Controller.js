d3f88814ec5e12ec446394d3a514875cd98994d7
Jhnny94 <johnnyn@getacademy.no>
2021-04-12 14:41:48 +0200

    Yes

--- ObliOppgave/Controller.js
+++ ObliOppgave/Controller.js
@@ -1,10 +1,20 @@
-Bruker1();
-function Bruker1()
-{
-
-
-
-
-
+//controller
+function btnlogin()
+{  
+    if (usr.value === navn && psw.value === password){
+    
+      afterLogin(); 
+    
+    }
+    else if (usr.value === navn2 && psw.value === password2){
+        afterLogin();
+    }
+        else  alert("Feil Passord");
 
 }
+
+function myFunction(){
+   return "Hello " + demo1;
+     
+}
+
--- ObliOppgave/Main.js
+++ ObliOppgave/Main.js
@@ -1,9 +1,11 @@
 //model
-var name = "Johnny";
+var navn = "Johnny";
 var password = "abc123";
-
-
-
+var navn2 = "Marius";
+var password2 = "hehe";
+var usr = "vilde";
+var psw = "vilde123";
+var demo1 = "Therese";
 //View
 updateNow();
 function updateNow()
@@ -13,22 +15,50 @@ function updateNow()
             <h2>Logg inn med brukernavn og passord </h2>
             
             <br>
-            <input class="form-controll" placeholder="Brukernavn"
+            <input onchange="usr = this.value" 
+            id="usr" class="form-controll" 
+            placeholder="Brukernavn">
             
         </div>
             <div> 
-            <input class="form-controll" placeholder="Passord"  
-            </div>
+            <input onchange="psw = this.value" 
+            id="psw" 
+            class="form-controll" 
+            placeholder="Passord"> 
+           
+             </div>
         <div> 
-            <button class="btn btn-lg btn-success btn-block" 
-            type="submit" name="btnlogin">Logg inn</button>
+            <button onClick="btnlogin()"
+            class="btn btn-lg btn-success btn-block" 
+            type="submit" name="Roal">Logg inn</button>
+            </div>
+            <br>
+        <div>
+            <button id="Somalia2"
+            onClick="myFunction(demo1)"> Return verdi Knappen</button>
+        </div>
     `;
-
-
 }
     
+function afterLogin() 
+{
+    document.getElementById("app").innerHTML = `
+    
+    <p onClick="myFunction2()" id="demo"> Hei </p>
+    `;
+   
+}
 
 
 
 
-//controller
+
+//Oppgaven skal inneholde: 
+
+//Skal være i Mvc (model, view, controller)
+//Minst 1 Eventhandler (onclick etc.)
+//Minst 1 If-setning  (med && eller || , eller begge)
+//Minst en funksjon som returnerer en verdi
+//Minst en funksjon som bruker parameter 
+//(ps. de to siste KAN være samme funksjon)
+//Minst 3 unit tester til sammen av funksjonene du bruker (ikke test mot view)
