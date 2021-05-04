4210242644f6ce9685d63c52ef5b1da906b1410f
Jhnny94 <johnnyn@getacademy.no>
2021-05-04 14:24:03 +0200

    uptade

--- myWork/Main.js
+++ myWork/Main.js
@@ -5,17 +5,4 @@
 
 
 
-//view
-uptadeNow()
-function uptadeNow();
-{
- document.getElementById("app").innerHTML=`
-<form class="box" action="index.html" method="post">
-    <h1>Login</h1>
-    <input type="text" name="" placeholder="Brukernavn">
-    <input type="password" name="" placeholder="Passord">
-    <input type="submit" name="" value="Login">;
-</form> 
-`; 
 
-}
---
+++ myWork/myWork2/Main2.js
@@ -0,0 +1,14 @@
+<div class="login-page">
+<div class="form">
+    <form class="register-form">
+    <input type="text" placeholder="password"/>
+    <input type="text" placeholder="user name"/>
+    <input type="text" placeholder="email id"/>
+    <button> Create</button>
+
+
+    </form>
+
+
+    </div>
+    </div>
